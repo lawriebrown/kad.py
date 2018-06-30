c01c095d49262eff47a0a7ee8e657574585ede06
Lawrie Brown <Lawrie.Brown@canb.auug.org.au>
2018-06-30 12:30:32 +1000

    Add locks and synchronize all access to DHT data and rpc_ids. Add some pydoc comments.

--- kad/kad.py
+++ kad/kad.py
@@ -1,11 +1,7 @@
 """ Simple python Kademlia DHT data store implementation.
 Useful for distributing a key-value store in a decentralized manner.
 
-To create a new DHT swarm, just call DHT() with the host and port
-that you will listen on. To join an existing DHT swarm, also provide
-bootstrap host and port numbers of any existing node.  The nodes
-will discover the rest of the swarm as appropriate during usage.
-
+To create a new DHT swarm, just call DHT() with the host and port that you will listen on. To join an existing DHT swarm, also provide bootstrap host and port numbers of any existing node.  The nodes will discover the rest of the swarm as appropriate during usage.
 """
 
 import json
@@ -25,7 +21,7 @@ from . import hashing
 
 # hack to handle long integers in both python2 & 3 (where just have int)
 if sys.version_info > (3,):
-    long = int
+	long = int
 
 k = 20
 alpha = 3
@@ -79,10 +75,13 @@ class DHTRequestHandler(socketserver.BaseRequestHandler):
 		client_host, client_port = self.client_address
 		peer = Peer(client_host, client_port, id, info)
 		response_socket = self.request[1]
+		self.server.dht.data_lock.acquire()
 		if find_value and (str(key) in self.server.dht.data):
 			value = self.server.dht.data[str(key)]
+			self.server.dht.data_lock.release()
 			peer.found_value(id, value, message["rpc_id"], socket=response_socket, peer_id=self.server.dht.peer.id, peer_info=self.server.dht.peer.info, lock=self.server.send_lock)
 		else:
+			self.server.dht.data_lock.release()
 			nearest_nodes = self.server.dht.buckets.nearest_nodes(id)
 			if not nearest_nodes:
 				nearest_nodes.append(self.server.dht.peer)
@@ -91,34 +90,54 @@ class DHTRequestHandler(socketserver.BaseRequestHandler):
 
 	def handle_found_nodes(self, message):
 		rpc_id = message["rpc_id"]
-		shortlist = self.server.dht.rpc_ids[rpc_id]
-		del self.server.dht.rpc_ids[rpc_id]
+		with self.server.dht.rpc_ids_lock:
+			shortlist = self.server.dht.rpc_ids[rpc_id]
+			del self.server.dht.rpc_ids[rpc_id]
 		nearest_nodes = [Peer(*peer) for peer in message["nearest_nodes"]]
 		shortlist.update(nearest_nodes)
 
 	def handle_found_value(self, message):
 		rpc_id = message["rpc_id"]
-		shortlist = self.server.dht.rpc_ids[rpc_id]
-		del self.server.dht.rpc_ids[rpc_id]
+		with self.server.dht.rpc_ids_lock:
+			shortlist = self.server.dht.rpc_ids[rpc_id]
+			del self.server.dht.rpc_ids[rpc_id]
 		shortlist.set_complete(message["value"])
 
 	def handle_store(self, message):
 		key = message["id"]
 		value = message["value"]
 		self.server.dht.logger.info('store ("%s",%s)' % (str(key),str(value)))
-		self.server.dht.data[str(key)] = value
+		with self.server.dht.data_lock:
+			self.server.dht.data[str(key)] = value
 
 
 class DHTServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+	""" UDP Server for a DHT instance listening on host:port.
+
+	Uses the specified handler class (default is DHTRequestHandler).
+	A reference to the DHT instance is saved in self.dht after creation
+	by DHT __init__ to allow the handler class to access the DHT data store.
+	"""
 	def __init__(self, host_address, handler_cls):
 		socketserver.UDPServer.__init__(self, host_address, handler_cls)
 		self.send_lock = threading.Lock()
 
 	def server_close(self):
-	        self.dht.logger.debug('server closing down')
-	        return socketserver.UDPServer.server_close(self)
+		self.dht.logger.debug('server closing down')
+		return socketserver.UDPServer.server_close(self)
 
 class DHT(object):
+	""" DHT node instance with associated server listening on host:port,
+	with a new random id (unless explicitly specified).
+
+	The server will connect to other node(s) listed in seeds to
+	bootstrap into an existing DHT swarm. If no seeds given, then
+	presumably are starting a new swarm.
+
+	Uses the specified storage (eg shelve or pysos, defaults to dict).
+	Can provide some option info details, distributed with requests.
+	The hashing function and requesthandler can also be over-ridden.
+	"""
 	def __init__(self, host, port, id=None, seeds=[], storage={}, info={}, hash_function=hashing.hash_function, requesthandler=DHTRequestHandler):
 		if not id:
 			id = random_id()
@@ -127,8 +146,10 @@ class DHT(object):
 		self.hash_function = hash_function
 		self.peer = Peer(host, port, id, info)
 		self.data = self.storage
+		self.data_lock = threading.Lock()
 		self.buckets = BucketSet(k, id_bits, self.peer.id)
-		self.rpc_ids = {} # should probably have a lock for this
+		self.rpc_ids = {}
+		self.rpc_ids_lock = threading.Lock()
 		self.server = DHTServer(self.peer.address(), requesthandler)
 		self.server.dht = self
 		self.logger = logging.getLogger('DHT(%s:%d)' % (self.peer.host, self.peer.port))
@@ -148,14 +169,16 @@ class DHT(object):
 		shortlist.update(self.buckets.nearest_nodes(key, limit=alpha))
 		if boot_peer:
 			rpc_id = random.getrandbits(id_bits)
-			self.rpc_ids[rpc_id] = shortlist
+			with self.rpc_ids_lock:
+				self.rpc_ids[rpc_id] = shortlist
 			boot_peer.find_node(key, rpc_id, socket=self.server.socket, peer_id=self.peer.id, peer_info=self.peer.info)
 		while (not shortlist.complete()) or boot_peer:
 			nearest_nodes = shortlist.get_next_iteration(alpha)
 			for peer in nearest_nodes:
 				shortlist.mark(peer)
 				rpc_id = random.getrandbits(id_bits)
-				self.rpc_ids[rpc_id] = shortlist
+				with self.rpc_ids_lock:
+					self.rpc_ids[rpc_id] = shortlist
 				peer.find_node(key, rpc_id, socket=self.server.socket, peer_id=self.peer.id, peer_info=self.info) ######
 			time.sleep(iteration_sleep)
 			boot_peer = None
@@ -169,7 +192,8 @@ class DHT(object):
 			for peer in nearest_nodes:
 				shortlist.mark(peer)
 				rpc_id = random.getrandbits(id_bits)
-				self.rpc_ids[rpc_id] = shortlist
+				with self.rpc_ids_lock:
+					self.rpc_ids[rpc_id] = shortlist
 				peer.find_value(key, rpc_id, socket=self.server.socket, peer_id=self.peer.id, peer_info=self.info) #####
 			time.sleep(iteration_sleep)
 		return shortlist.completion_result()
@@ -210,7 +234,9 @@ class DHT(object):
 
 	# Iterator
 	def __iter__(self):
-		return iter(map(lambda key: long(key), self.data.__iter__()))
+		with self.data_lock:
+			data_iter = iter(map(lambda key: long(key), self.data.__iter__()))
+		return data_iter
 
 	# Operator []
 	def __getitem__(self, key):
@@ -218,10 +244,14 @@ class DHT(object):
 			hashed_key = key
 		else:
 			hashed_key = self.hash_function(key)
+		self.data_lock.acquire()
 		if str(hashed_key) in self.data:
 			result = self.data[str(hashed_key)]
+			self.data_lock.release()
 			self.logger.debug('get("%s") at %d is %s' % (key, hashed_key, str(result)))
 			return result
+		else:	
+			self.data_lock.release()
 		result = self.iterative_find_value(hashed_key)
 		if result:
 			self.logger.debug('get("%s") at %d is %s' % (key, hashed_key, str(result)))
@@ -234,7 +264,8 @@ class DHT(object):
 		self.logger.info('set("%s",%s) at %d' % (key, str(value), hashed_key))
 		nearest_nodes = self.iterative_find_nodes(hashed_key)
 		if not nearest_nodes:
-			self.data[str(hashed_key)] = value
+			with self.data_lock:
+				self.data[str(hashed_key)] = value
 		for node in nearest_nodes:
 			node.store(hashed_key, value, socket=self.server.socket, peer_id=self.peer.id)
 
